361d1476d9bcb2491475684856910ccc38974f3c
Lucas Henry <henrylucas755@gmail.com>
2024-07-08 20:23:46 -0400

    basic messaging

--- frontend-solo-travel-project/src/components/inbox/MainInbox.jsx
+++ frontend-solo-travel-project/src/components/inbox/MainInbox.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import MessageSidebar from './MessageSidebar'
-import ChatBox from './ChatBox'
-import { Box } from '@mui/material'
+import React from "react";
+import MessageSidebar from "./MessageSidebar";
+import ChatBox from "./ChatBox";
+import { Box } from "@mui/material";
 
-function MainInbox() {
-    return (
-        <Box sx={{ height: "95vh", width: "87.5vw", display: "flex", pt: "68px"}}>
-            <Box sx={{ flex: "1", maxWidth: "300px" }}>
-                <MessageSidebar />
-            </Box>
-            <Box sx={{ flex: "3", height: "100%"}}>
-                <ChatBox />
-            </Box>
-        </Box>
-    );
+function MainInbox({ token }) {
+  return (
+    <Box sx={{ height: "95vh", width: "87.5vw", display: "flex", pt: "68px" }}>
+      <Box sx={{ flex: "1", maxWidth: "300px" }}>
+        <MessageSidebar />
+      </Box>
+      <Box sx={{ flex: "3", height: "100%" }}>
+        <ChatBox token={token} />
+      </Box>
+    </Box>
+  );
 }
 
-export default MainInbox
+export default MainInbox;
