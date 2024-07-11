b2db203467e330f342b9133cb2ecc8fc9d36889c
Lucas Henry <henrylucas755@gmail.com>
2024-07-11 19:02:52 -0400

    message push

--- frontend-solo-travel-project/src/components/inbox/ChatBox.jsx
+++ frontend-solo-travel-project/src/components/inbox/ChatBox.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import MessageCard from "./MessageCard";
+import { Avatar, Box, Button, IconButton } from "@mui/material";
 import {
   MainContainer,
   ChatContainer,
