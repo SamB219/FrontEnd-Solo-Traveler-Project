b182dba5e4d26501e7205afa19fbdb492f343936
Lucas Henry <henrylucas755@gmail.com>
2024-07-16 12:14:02 -0400

    added pin modals

--- frontend-solo-travel-project/src/components/maps/Pin.jsx
+++ frontend-solo-travel-project/src/components/maps/Pin.jsx
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Marker, Popup } from "react-leaflet";
 /* import Pin from "../maps/Pin-location.png"; */
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import PinModals from "./PinModals";
 
 function Pin({ posts }) {
-  const myIcon = L.icon({
-    iconUrl: icon, // icon png
-    iconSize: [25, 30], // size of the icon
-    iconAnchor: [12.5, 41], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, -41], // point from which the popup should open relative to the iconAnchor
-  });
-
-  L.Marker.prototype.options.icon = myIcon;
-
   return (
     <>
       {posts.map((post) => {
         let coordinates = [post.location[3], post.location[2]];
         return (
-          <Marker position={coordinates}>
-            <Popup>{post.title}</Popup>
-          </Marker>
+          <>
+            <PinModals post={post} coordinates={coordinates} />
+          </>
         );
       })}
     </>
